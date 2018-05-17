1f2ae75825dd01a9c372bca055748379f852fdcc
Vitali Sharstuk <Vitali_Sharstuk@epam.com>
2018-05-17 08:43:37 +0300

    Module 2

--- src/main/java/ua/epam/spring/hometask/service/impl/AuditoriumServiceImpl.java
+++ src/main/java/ua/epam/spring/hometask/service/impl/AuditoriumServiceImpl.java
@@ -1,23 +1,27 @@
 package ua.epam.spring.hometask.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ua.epam.spring.hometask.dao.AuditoriumDao;
 import ua.epam.spring.hometask.domain.Auditorium;
 import ua.epam.spring.hometask.service.AuditoriumService;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.Set;
 
-
+@Service
 public class AuditoriumServiceImpl implements AuditoriumService {
 
+    @Autowired
     private AuditoriumDao auditoriumDao;
 
-    public AuditoriumServiceImpl(AuditoriumDao auditoriumDao)
+   /* public AuditoriumServiceImpl(AuditoriumDao auditoriumDao)
     {
 
         this.auditoriumDao = auditoriumDao;
-    }
+    }*/
 
     @Nonnull
     @Override
